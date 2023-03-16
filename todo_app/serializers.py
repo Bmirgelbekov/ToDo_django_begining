d9ef0790a89ef59744064011956f1835ea61baa7
bektur <1903.03005@manas.edu.kg>
2023-03-16 19:58:45 +0600

    added functions

---
+++ todo_app/serializers.py
@@ -0,0 +1,27 @@
+from rest_framework import serializers
+
+from .models import ToDo
+
+class ToDoSerializers(serializers.Serializer):
+    id = serializers.ReadOnlyField()
+    title = serializers.CharField(max_length=200)
+    is_completed = serializers.BooleanField(required=False)
+    created_at = serializers.ReadOnlyField()
+    deadline = serializers.DateTimeField()
+
+
+class UpdateToDoSerializer(serializers.Serializer):
+    title = serializers.CharField(required = False, max_length=200)
+    deadline = serializers.DateTimeField(required=False)
+    is_completed = serializers.BooleanField(required=False)
+
+    def update(self, instance: ToDo, validated_data: dict) -> ToDo:
+        print(validated_data)
+        for key, value in validated_data.items():
+            setattr(instance, key, value)
+            
+        # instance.title = validated_data.get('title', instance.title)
+        # instance.deadline = validated_data.get('deadline', instance.deadline)
+        # instance.is_completed = validated_data.get('is_completed', instance.is_completed)
+        instance.save()
+        return instance
--- todo_app/views.py
+++ todo_app/views.py
@@ -1,3 +1,76 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.request import Request
+from rest_framework.response import Response
+from rest_framework import status
+from django.shortcuts import get_object_or_404
+from drf_yasg.utils import swagger_auto_schema
 
-# Create your views here.
+from .models import ToDo
+from .serializers import ToDoSerializers, UpdateToDoSerializer
+
+@api_view(['GET'])
+def get_todos(request: Request):
+
+    queryset = ToDo.objects.all()
+    # SELECT * FROM todo -> QuerySet([{title..., desc...}])
+    print(queryset)
+    serializer = ToDoSerializers(queryset, many=True)
+    print(serializer.data)
+    return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+@api_view(['GET'])
+def retreve(request: Request, pk):
+    try:
+        queryset = ToDo.objects.get(pk=pk)
+    except ToDo.DoesNotExist:
+        return Response({'detail': f'Object {pk} does not exist'}, status=status.HTTP_404_NOT_FOUND)
+    serializer = ToDoSerializers(queryset)
+    return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+@api_view(['GET'])
+def get_todo(request: Request, pk):
+    todo = get_object_or_404(ToDo, pk=pk)
+    serializer = ToDoSerializers(instance=todo)
+    return Response(serializer.data, status=status.HTTP_200_OK)
+
+@swagger_auto_schema(method='POST', request_body=ToDoSerializers)
+@api_view(['POST'])
+def create_todo(request: Request):
+    serializer = ToDoSerializers(data=request.data)
+    serializer.is_valid(raise_exception=True)
+    todo = ToDo.objects.create(**serializer.data)
+    todo.save()
+    return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    # if serializer.is_valid():
+    #     todo = ToDo.objects.create(**serializer.data)
+    #     todo.save()
+    #     return Response(serializer.data, status=status.HTTP_201_CREATED)
+    # return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@swagger_auto_schema(method='PATCH', request_body=UpdateToDoSerializer)
+@api_view(['PATCH'])
+def update_todo(request: Request, pk) -> Response:
+    print(pk)
+    try:
+        todo = ToDo.objects.get(pk=pk)
+    except ToDo.DoesNotExist:
+        return Response({'detail': f'ToDo with {pk} does not exist'}, status=status.HTTP_404_NOT_FOUND)
+    serializer = UpdateToDoSerializer(instance=todo, data=request.data)
+    serializer.is_valid(raise_exception=True)
+    serializer.save()
+    return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+@api_view(['DELETE'])
+def delete_todo(request: Request, pk) -> Response:
+    try:
+        todo = ToDo.objects.get(pk=pk)
+    except ToDo.DoesNotExist:
+        return Response({'detail': f'ToDo with {pk} does not exist'}, status=status.HTTP_404_NOT_FOUND)
+    todo.delete()
+    return Response({'detail': 'Successfully deleted'}, status=status.HTTP_204_NO_CONTENT)
+
+#
